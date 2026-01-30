10c3aadf8fb702925d275e213b08799badd1aeed
Christina Stamminger <christina-stamminger@gmail.com>
2026-01-30 18:50:36 +0100

    index fixed

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-import 'react-native-gesture-handler'; // MUSS GANZ OBEN SEIN
+import 'react-native-gesture-handler'; // 🔥 MUSS GANZ OBEN SEIN
 
 import { registerRootComponent } from 'expo';
 import App from './App';
 
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App)
 registerRootComponent(App);
 
-import { registerRootComponent } from 'expo';
-
-import App from './App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
