1092b8394e66491d5834726830b4ac655f9c6018
Christina Stamminger <christina-stamminger@gmail.com>
2026-03-18 09:44:42 +0100

    fix env setup

--- app.config.js
+++ app.config.js
@@ -1,6 +1,20 @@
 import "dotenv/config";
 
-const appVariant = process.env.APP_VARIANT ?? "production";
+const appVariant = process.env.APP_VARIANT;
+const appEnv = process.env.APP_ENV;
+const apiUrl = process.env.API_URL;
+
+if (!appVariant) {
+  throw new Error("APP_VARIANT is missing");
+}
+
+if (!appEnv) {
+  throw new Error("APP_ENV is missing");
+}
+
+if (!apiUrl) {
+  throw new Error("API_URL is missing");
+}
 
 const getAppName = () => {
   if (appVariant === "development") return "bringit Dev";
@@ -22,28 +36,23 @@ export default {
     cli: {
       appVersionSource: "local",
     },
-
     runtimeVersion: {
       policy: "appVersion",
     },
-
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
     newArchEnabled: true,
-
     updates: {
       url: "https://u.expo.dev/da762c57-1d88-4aba-b0cb-d5c4fb973bdb",
       checkAutomatically: "ON_LOAD",
       fallbackToCacheTimeout: 0,
     },
-
     splash: {
       image: "./assets/splash-icon.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff",
     },
-
     ios: {
       supportsTablet: true,
       bundleIdentifier: getBundleIdentifier(),
@@ -52,7 +61,6 @@ export default {
         ITSAppUsesNonExemptEncryption: false,
       },
     },
-
     android: {
       package: getBundleIdentifier(),
       versionCode: 15,
@@ -62,11 +70,9 @@ export default {
         backgroundColor: "#ffffff",
       },
     },
-
     web: {
       favicon: "./assets/favicon.png",
     },
-
     plugins: [
       "expo-secure-store",
       [
@@ -81,13 +87,11 @@ export default {
         },
       ],
     ],
-
     scheme: "bringit",
     platforms: ["ios", "android", "web"],
-
     extra: {
-      API_URL: process.env.API_URL,
-      APP_ENV: process.env.APP_ENV,
+      API_URL: apiUrl,
+      APP_ENV: appEnv,
       APP_VARIANT: appVariant,
       eas: {
         projectId: "da762c57-1d88-4aba-b0cb-d5c4fb973bdb",
